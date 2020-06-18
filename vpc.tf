ed0cb726a018be7140c668b8be8e10d9d8c90f0b
erinlslo <erin.ls.lo@gmail.com>
2020-06-18 09:26:27 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,8 @@
 module "vpc" {
   source  = "app.terraform.io/erin-training/vpc/aws"
   version = "2.36.0"
-  cidr_block = "10.0.0.0/16"
 }
 
-# resource "aws_vpc" "main" {
-#   cidr_block = "10.0.0.0/16"
-# }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
